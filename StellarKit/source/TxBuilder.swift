45103b20bf08aa83efa612ba59a468152db2bf6a
ashevin <avi@kik.com>
2019-04-14 09:07:56 +0300

    Bug fix.

--- StellarKit/source/TxBuilder.swift
+++ StellarKit/source/TxBuilder.swift
@@ -24,7 +24,7 @@ public final class TxBuilder {
 
         signers = [source]
 
-        _tx = tx ?? Transaction(sourceAccount: source.stellarKey,
+        _tx = tx ?? Transaction(sourceAccount: StellarKey(source.publicKey)!,
                                 seqNum: 0,
                                 timeBounds: nil,
                                 memo: .MEMO_NONE,
